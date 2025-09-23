54dc345b9c486a4bb2608bdc5ed433110fc9e275
hyeopsang <dbtkdguq3@gmail.com>
2025-09-23 16:24:10 +0900

    style(home): aboutcard 텍스트 스타일 수정

--- src/feature/home/about-card.tsx
+++ src/feature/home/about-card.tsx
@@ -1,10 +1,16 @@
 import clsx from "clsx";
 
 interface AboutCardProps {
+  title: string;
+  description: string;
   reverse?: boolean;
 }
 
-export default function AboutCard({ reverse = false }: AboutCardProps) {
+export default function AboutCard({
+  title,
+  description,
+  reverse = false,
+}: AboutCardProps) {
   return (
     <div
       className={clsx(
@@ -12,8 +18,19 @@ export default function AboutCard({ reverse = false }: AboutCardProps) {
         reverse && "2xl:flex-row-reverse 3xl:flex-row-reverse"
       )}
     >
-      <div className="rounded-[20px] bg-gray-100 w-full 2xl:w-1/2 aspect-square"></div>
-      <div className="w-full 2xl:w-1/2 aspect-square"></div>
+      {/* 이미지나 타이틀 영역 */}
+      <div className="rounded-[20px] bg-gray-100 w-full 2xl:w-1/2 aspect-square flex justify-center items-center text-xl font-semibold"></div>
+
+      {/* 설명 영역 */}
+      <div className="w-full 2xl:w-1/2 gap-[15px] aspect-square flex flex-col justify-center items-center text-left p-4">
+        <h3 className="3xl:w-80 w-full text-left text-2xl 2xl:text-[28px] 3xl:text-[32px] text-black font-bold">
+          {title}
+        </h3>
+
+        <p className="3xl:w-80 w-full text-wrap text-[18px] 2xl:text-[22px] text-[#999] font-medium">
+          {description}
+        </p>
+      </div>
     </div>
   );
 }
--- src/feature/home/about.tsx
+++ src/feature/home/about.tsx
@@ -3,9 +3,20 @@ import AboutCard from "./about-card";
 export default function About() {
   return (
     <section className="w-full py-10 2xl:py-20">
-      <AboutCard reverse />
-      <AboutCard />
-      <AboutCard reverse />
+      <AboutCard
+        title="유연한 드로잉 캔버스"
+        description="브러시로 자유롭게 그리기, 색상 선택, 선 두께 조절, 지우개 사용, 그리고 스케치를 이미지로 저장까지"
+        reverse
+      />
+      <AboutCard
+        title="글 속에 스케치 삽입"
+        description="직접 스케치 한 이미지를 글에 삽입하여 아이디어를 시각적으로 표현"
+      />
+      <AboutCard
+        reverse
+        title="다양한 글쓰기 기능 및 히스토리 지원"
+        description="마크다운 지원, 글 버전 관리, 자동 저장 기능으로 안전하게 글 작성"
+      />
     </section>
   );
 }
