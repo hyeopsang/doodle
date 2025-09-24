a91a5a68e443f3f096a8e04307f9f1715e98274e
hyeopsang <dbtkdguq3@gmail.com>
2025-09-24 22:45:45 +0900

    style: 헤더 zindex 추가

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import DarkModePicker from './dark-mode-picker';
 import {Link} from 'react-router';
 export default function Header() {
   return (
-    <header className="w-full fixed h-16 flex justify-center items-center bg-white dark:bg-black">
+    <header className="w-full fixed h-16 flex justify-center items-center bg-white dark:bg-black z-50">
       <div className="max-w-[960px] w-full 3xl:px-0 2xl:px-[20px] xs:px-[7.5px] flex justify-between items-center">
         <Link to={'/'} className="text-black dark:text-white">
           <Doodle width={124} height={43} />
