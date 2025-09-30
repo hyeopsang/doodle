bbc97d008034b5d1cc8f1e70ac928652bb8e45f0
hyeopsang <dbtkdguq3@gmail.com>
2025-09-30 17:17:41 +0900

    style: layout css 수정

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout() {
   return (
     <div className="w-full h-dvh flex flex-col justify-between bg-white dark:bg-black items-center">
       <Header />
-      <main>
+      <main className="pt-16 responsive-layout">
         <Outlet />
       </main>
       <Footer />
